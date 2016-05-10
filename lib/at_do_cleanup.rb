653616c5c8cf1ad0d4c8e0324cf6b20239e6679e
jgpawletko <jgpawletko@gmail.com>
2016-05-10 11:19:28 -0400

    refactor: move DB constants to module

---
+++ lib/at_do_cleanup.rb
@@ -0,0 +1,4 @@
+require_relative './at_do_cleanup/config'
+module ATDOCleanup
+  VERSION = '0.1.0'
+end
---
+++ lib/at_do_cleanup/config.rb
@@ -0,0 +1,11 @@
+module ATDOCleanup
+  # database table names, attribute names, and expected values
+  DO_TABLE              = 'DigitalObjects'.freeze
+  DO_ID_ATTR            = 'digitalObjectId'.freeze
+  METS_ID_ATTR          = 'metsIdentifier'.freeze
+  ARCH_INST_ID_ATTR     = 'archDescriptionInstancesId'.freeze
+  CREATED_BY_ATTR       = 'createdBy'.freeze
+  LAST_UPDATED_BY_ATTR  = 'lastUpdatedBy'.freeze
+  CREATED_BY_VALUE      = 'dlts'.freeze
+  LAST_UPDATED_BY_VALUE = 'dlts'.freeze
+end
