fd51d2d03b44a55933f6619647bae09519895855
jgpawletko <jgpawletko@gmail.com>
2016-05-12 12:52:45 -0400

    add comments to FileVersion class

--- lib/at_do_cleanup/file_version.rb
+++ lib/at_do_cleanup/file_version.rb
@@ -3,10 +3,18 @@ require 'ostruct'
 module ATDOCleanup
   # class stores attributes of DigitalObject records
   class FileVersion
+    # return an OpenStruct for the record
     def self.new(params)
       OpenStruct.new(params)
     end
 
+    # .find_records_for_digital_object
+    # purpose:
+    #   returns all records in FV_TABLE with DO_ID_ATTR = args[:do_id]
+    #
+    # arguments:
+    #   args[:client] = database client object to which queries are passed
+    #   args[:do_id]  = an integer used as a foreign key into FV_TABLE
     def self.find_records_for_digital_object(args)
       client = args[:client]
       digital_object_id = args[:do_id]
@@ -19,6 +27,13 @@ WHERE #{DO_ID_ATTR} = #{digital_object_id}"
       client.query(query)
     end
 
+    # .delete
+    # purpose:
+    #   delete a record from FV_TABLE using the FILE_VERSION_ID_ATTR attribute
+    #
+    # arguments:
+    #   args[:client] = database client object to which queries are passed
+    #   args[:file_version] = an object that responds to .send(FILE_VERSION_ID_ATTR)
     def self.delete(args)
       client = args[:client]
       file_version = args[:file_version]
@@ -31,5 +46,5 @@ WHERE #{FILE_VERSION_ID_ATTR} = #{file_version.send(FILE_VERSION_ID_ATTR)}"
       puts query
       client.query(query)
     end
-end
+  end
 end
