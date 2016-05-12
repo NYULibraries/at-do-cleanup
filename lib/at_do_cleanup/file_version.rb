71dd22b0bb5e0ae9b02122167f628d8ea1583990
jgpawletko <jgpawletko@gmail.com>
2016-05-12 12:45:05 -0400

    add FileVersion.delete

--- lib/at_do_cleanup/file_version.rb
+++ lib/at_do_cleanup/file_version.rb
@@ -18,5 +18,18 @@ WHERE #{DO_ID_ATTR} = #{digital_object_id}"
 
       client.query(query)
     end
-  end
+
+    def self.delete(args)
+      client = args[:client]
+      file_version = args[:file_version]
+      raise ArgumentError, 'missing parameter file_version:' unless file_version
+
+      query = "DELETE \
+FROM #{FV_TABLE} \
+WHERE #{FILE_VERSION_ID_ATTR} = #{file_version.send(FILE_VERSION_ID_ATTR)}"
+
+      puts query
+      client.query(query)
+    end
+end
 end
