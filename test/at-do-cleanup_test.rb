ddc1dff12c7e62a2f5a461c618abbd1ff9ce76bb
jgpawletko <jgpawletko@gmail.com>
2016-05-23 11:44:04 -0400

    clean up test title and output

--- test/at-do-cleanup_test.rb
+++ test/at-do-cleanup_test.rb
@@ -6,10 +6,10 @@ class TestAtDoCleanup < MiniTest::Test
   COMMAND = 'bin/at-do-cleanup'.freeze
   EXPECTED_OUTPUT = `cat test/fixtures/output.txt`.freeze
 
-  def test_exit_status_with_valid_text
+  def test_output_and_status
     o, e, s = Open3.capture3(COMMAND)
     assert(s.exitstatus == 0, 'incorrect exit status')
-    assert_equal(EXPECTED_OUTPUT, o, 'output mismatch')
-    assert_equal('', e, "error text: #{e}")
+    assert_equal(EXPECTED_OUTPUT, o, 'stdout output mismatch')
+    assert_equal('', e, 'stderr output mismatch')
   end
 end
