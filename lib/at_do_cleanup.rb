0399d8581f5201bcac437d940da684cff903e8a2
jgpawletko <jgpawletko@gmail.com>
2016-06-29 15:45:19 -0400

    add AuthRecordError

--- lib/at_do_cleanup.rb
+++ lib/at_do_cleanup.rb
@@ -3,6 +3,7 @@ require_relative './at_do_cleanup/config'
 require_relative './at_do_cleanup/digital_object'
 require_relative './at_do_cleanup/client'
 require_relative './at_do_cleanup/cleaner'
+require_relative './at_do_cleanup/auth_record_error'
 
 module ATDOCleanup
   VERSION = '0.1.0'.freeze
