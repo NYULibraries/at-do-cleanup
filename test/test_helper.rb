99d037425982df647de76d878f24777add54b05d
jgpawletko <jgpawletko@gmail.com>
2016-07-03 16:19:29 -0400

    add DUPE_HASH_WITH_PROTECTED_URI hash

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -51,3 +51,29 @@ AUTHORITATIVE_HASH = {
   'componentId' => '',
   'repositoryId' => 2
 }.freeze
+
+DUPE_HASH_WITH_PROTECTED_URI = {
+  'digitalObjectId' => 8343,
+  'version' => 1,
+  'lastUpdated' => '2013-03-07 11:19:45 -0500',
+  'created' => '2012-09-27 13:43:55 -0400',
+  'lastUpdatedBy' => 'dlts',
+  'createdBy' => 'dlts',
+  'title' => 'The baracon: Nela',
+  'dateExpression' => '1948',
+  'dateBegin' => 1948,
+  'dateEnd' => 1948,
+  'languageCode' => '',
+  'restrictionsApply' => '\x00',
+  'eadDaoActuate' => '',
+  'eadDaoShow' => '',
+  'metsIdentifier' => 'RISM MC 1.ref197.1',
+  'objectType' => '',
+  'label' => '',
+  'objectOrder' => 0,
+  'parentDigitalObjectId' => nil,
+  'archDescriptionInstancesId' => nil,
+  'componentId' => '',
+  'repositoryId' => 2,
+  'file_version_uri' => 'http://webarchives.cdlib.org/sw1kp7tt2d/http://www.chomsky.info/'
+}.freeze
